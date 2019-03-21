9b2dea25146a5e492c4c4fcc685b5bd187f4bf50
caglaror <caglaror@gmail.com>
2019-03-20 23:36:08 -0700

    -CSS transitions added. -Audio events added (play, end)

--- js/index.js
+++ js/index.js
@@ -6,7 +6,7 @@ window.addEventListener('load', function(){
 
 function loaded(){
     window.addEventListener('keydown', function(e){
-        console.log(e.key + ' key pressed!');
+        //console.log(e.key + ' key pressed!');
         playTheSound(e.key)
 
     })
@@ -15,20 +15,36 @@ function loaded(){
         elem.addEventListener('click', function (e) {
 
             let thatKey = e.target.dataset.key? e.target.dataset.key : e.target.parentNode.dataset.key;
-            console.log(thatKey+ ' key clicked.');
-            playTheSound(thatKey)
+            //console.log(thatKey+ ' key clicked.');
+            playTheSound(thatKey);
 
-        })
+        });
+
     })
 }
 
 function playTheSound(theKey){
     if(keyDrumMap.has(theKey)){
-        console.log('This key exist on our Map');
+        //console.log('This key exist on our Map');
         // Lets play the sound
         let newAudio = document.createElement('AUDIO');
+        //newAudio.id = theKey;
         newAudio.src = wavWay + keyDrumMap.get(theKey)+'.wav';
         newAudio.play();
+        newAudio.addEventListener('ended', function(newAudio){
+            document.querySelector(`.key[data-key=${theKey}]`).classList.remove('playing');
+            newAudio =null;
+        });
+        newAudio.addEventListener('play',function(){
+            if(document.querySelector(`.key[data-key=${theKey}]`).style.webkitAnimationPlayState=='running'){
+                document.querySelector(`.key[data-key=${theKey}]`).classList.remove('playing');
+            }
+                document.querySelector(`.key[data-key=${theKey}]`).classList.add('playing');
+
+
+        });
+
+
     }else{
         console.log('This key does not exist on our Map.')
     }
